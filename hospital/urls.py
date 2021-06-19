ac6a7ea5e51cfc322c7ae7bb19c530a6d1c47ebc
Yarosalw <yarosalw274@gmail.com>
2021-06-19 20:37:59 +0300

    Update

--- hospital/urls.py
+++ hospital/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('api/View_Doctor_signup', views.View_Doctor_signup.as_view(), name='get-View_Doctor_signup'),
-    path('api/View_Patient_signup', views.View_Patient_signup.as_view(), name='get-View_Patient_signup'),
+    path('api/View_Patient_signup', views.View_Patient_signup, name='get-View_Patient_signup'),
     path('api/View_data_Patient', views.View_data_Patient.as_view(), name='get-View_data_Patient'),
 
 ]
